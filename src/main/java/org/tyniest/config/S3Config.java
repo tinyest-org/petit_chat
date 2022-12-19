f51229fc5ec4fa1142cb5dfa1d24d011bcfa4042
Plawn <plawn.yay@gmail.com>
2022-12-19 14:48:04 +0100

    add seaweed

--- src/main/java/org/tyniest/config/S3Config.java
+++ src/main/java/org/tyniest/config/S3Config.java
@@ -1,8 +1,11 @@
 package org.tyniest.config;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.minio.MinioAsyncClient;
+import seaweedfs.client.FilerClient;
 
 public class S3Config {
     @ConfigProperty(name = "s3.host")
@@ -14,11 +17,20 @@ public class S3Config {
     @ConfigProperty(name = "s3.passkey")
     String passkey;
 
+    @ConfigProperty(name = "s3.filer", defaultValue = "")
+    String seaweedFiler;
 
+    @ApplicationScoped
     public MinioAsyncClient makeS3() {
         return MinioAsyncClient.builder()
             .endpoint(host)
             .credentials(accessKey, passkey)
             .build();
+    }
+
+    @ApplicationScoped
+    public FilerClient makeFiler() { // TODO: not used at the moment
+        final var filerClient = new FilerClient("localhost", 18888);
+        return filerClient;
     }
 }
