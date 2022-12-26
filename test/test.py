ece3713bb23d9107f6c41641362fa968a82c014b
Paul Leveau <plawn.yay@gmail.com>
2022-12-26 17:46:32 +0100

    can now add and remove reactions

--- test/test.py
+++ test/test.py
@@ -3,20 +3,20 @@ import datetime
 
 url = "http://localhost:8090"
 
-uid = '43c0db5c-d829-4929-8efc-5e4a13bb202f'
-
+chat_id = '43c0db5c-d829-4929-8efc-5e4a13bb202f'
+signal_id = "427bf120-8520-11ed-805d-0933ac6bdfd8"
 
 def get_msg(after: str = None):
     if after is None:
-        r = requests.get(f'{url}/chat/{uid}')
+        r = requests.get(f'{url}/chat/{chat_id}')
     else:
-        r = requests.get(f'{url}/chat/{uid}?lastMessage={after}')
+        r = requests.get(f'{url}/chat/{chat_id}?lastMessage={after}')
     print(r)
     print(r.text)
 
 
 def new_msg(payload: str):
-    r = requests.post(f'{url}/chat/{uid}', json={"content": payload})
+    r = requests.post(f'{url}/chat/{chat_id}', json={"content": payload})
     print(r)
     print(r.text)
 
@@ -28,7 +28,7 @@ def get_chat_for_user():
 
 
 def scenario_msg():
-    get_msg(after="427bf120-8520-11ed-805d-0933ac6bdfd8")
+    get_msg(after=signal_id)
     new_msg(datetime.datetime.now().isoformat())
 
 
@@ -44,9 +44,14 @@ def get_user_by_name():
 
 
 def get_users_in_chat():
-    r = requests.get(f'{url}/chat/{uid}/users')
+    r = requests.get(f'{url}/chat/{chat_id}/users')
     print(r)
     print(r.text)
 
+def add_reaction():
+    r = requests.put(f'{url}/chat/{chat_id}/{signal_id}/test')
+    print(r)
+    print(r.text)
 
-scenario_msg()
+add_reaction()
+get_msg(after="44a84700-8520-11ed-805d-0933ac6bdfd8")
