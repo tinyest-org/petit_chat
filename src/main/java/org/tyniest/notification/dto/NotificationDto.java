5fd6de22b58740ab5750f3de5da5c45a5e58c3ff
Paul Leveau <plawn.yay@gmail.com>
2023-03-12 11:13:22 +0100

    fix field name

--- src/main/java/org/tyniest/notification/dto/NotificationDto.java
+++ src/main/java/org/tyniest/notification/dto/NotificationDto.java
@@ -8,7 +8,7 @@ import lombok.Value;
 @Value(staticConstructor = "of")
 public class NotificationDto {
     private String content;
-    private NotificationType notificationType;
+    private NotificationType subject;
 
     public static NotificationDto of(NotificationType notificationType, Object o) {
         return NotificationDto.of(JsonRenderer.toJSON(o), notificationType);
